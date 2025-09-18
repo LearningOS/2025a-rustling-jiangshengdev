58cc7b2340de1b91d076ef6e12da1a2846f34d5b
Jiang Sheng <jiangshengdev@outlook.com>
2025-09-18 18:58:43 +0800

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,17 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        s.split_once(',')
+            .filter(|(name, _)| !name.is_empty())
+            .and_then(|(name, age)| {
+                age.parse::<usize>().ok().map(|age| Person {
+                    name: name.to_string(),
+                    age,
+                })
+            })
+            .unwrap_or_default()
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        match s {
+            "" => Err(ParsePersonError::Empty),
+            s => s
+                .split_once(',')
+                .filter(|(_, a)| !a.contains(','))
+                .ok_or(ParsePersonError::BadLen)
+                .and_then(|(n, a)| match n.is_empty() {
+                    true => Err(ParsePersonError::NoName),
+                    false => a
+                        .parse()
+                        .map(|age| Person {
+                            name: n.to_string(),
+                            age,
+                        })
+                        .map_err(ParsePersonError::ParseInt),
+                }),
+        }
     }
 }
 
