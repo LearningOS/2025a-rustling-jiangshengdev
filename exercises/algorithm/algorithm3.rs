bba3ef658301d0a8145b14cf07b0b455bda948e5
Jiang Sheng <jiangshengdev@outlook.com>
2025-09-19 18:07:33 +0800

    algorithm

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -3,10 +3,52 @@
 	This problem requires you to implement a sorting algorithm
 	you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn merge<T: PartialOrd + Clone>(array: &mut [T], left: usize, mid: usize, right: usize) {
+    let left_array = array[left..mid].to_vec();
+    let right_array = array[mid..right].to_vec();
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = left;
+    while i < left_array.len() && j < right_array.len() {
+        if left_array[i] <= right_array[j] {
+            array[k] = left_array[i].clone();
+            i += 1;
+        } else {
+            array[k] = right_array[j].clone();
+            j += 1;
+        }
+        k += 1;
+    }
+
+    while i < left_array.len() {
+        array[k] = left_array[i].clone();
+        i += 1;
+        k += 1;
+    }
+
+    while j < right_array.len() {
+        array[k] = right_array[j].clone();
+        j += 1;
+        k += 1;
+    }
+}
+
+fn merge_sort<T: PartialOrd + Clone>(array: &mut [T], left: usize, right: usize) {
+    if left + 1 < right {
+        let mid = left + (right - left) / 2;
+
+        merge_sort(array, left, mid);
+        merge_sort(array, mid, right);
+        merge(array, left, mid, right);
+    }
+}
+
+pub fn sort<T: PartialOrd + Clone>(array: &mut [T]) {
+    if array.len() > 1 {
+        merge_sort(array, 0, array.len());
+    }
 }
 #[cfg(test)]
 mod tests {
