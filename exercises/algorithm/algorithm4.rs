c2b86cc500fa0c4e7ca119f9389f1e40f6cdb934
Jiang Sheng <jiangshengdev@outlook.com>
2025-09-20 12:33:54 +0800

    algorithm

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -1,11 +1,10 @@
 /*
-	binary_search tree
-	This problem requires you to implement a basic interface for a binary tree
+    binary_search tree
+    This problem requires you to implement a basic interface for a binary tree
 */
 
 use std::cmp::Ordering;
 use std::fmt::Debug;
-
 
 #[derive(Debug)]
 struct TreeNode<T>
@@ -68,18 +67,14 @@ where
     fn insert(&mut self, value: T) {
         match value.cmp(&self.value) {
             Ordering::Equal => (), // Don't insert duplicate values
-            Ordering::Less => {
-                match &mut self.left {
-                    Some(left_node) => left_node.insert(value),
-                    None => self.left = Some(Box::new(TreeNode::new(value))),
-                }
-            }
-            Ordering::Greater => {
-                match &mut self.right {
-                    Some(right_node) => right_node.insert(value),
-                    None => self.right = Some(Box::new(TreeNode::new(value))),
-                }
-            }
+            Ordering::Less => match &mut self.left {
+                Some(left_node) => left_node.insert(value),
+                None => self.left = Some(Box::new(TreeNode::new(value))),
+            },
+            Ordering::Greater => match &mut self.right {
+                Some(right_node) => right_node.insert(value),
+                None => self.right = Some(Box::new(TreeNode::new(value))),
+            },
         }
     }
 
@@ -93,7 +88,6 @@ where
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -102,24 +96,20 @@ mod tests {
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
 
-        
         assert_eq!(bst.search(1), false);
 
-        
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
         assert_eq!(bst.search(7), true);
         assert_eq!(bst.search(2), true);
         assert_eq!(bst.search(4), true);
 
-        
         assert_eq!(bst.search(1), false);
         assert_eq!(bst.search(6), false);
     }
@@ -128,22 +118,17 @@ mod tests {
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
 
-        
         bst.insert(1);
         bst.insert(1);
 
-        
         assert_eq!(bst.search(1), true);
 
-        
         match bst.root {
             Some(ref node) => {
                 assert!(node.left.is_none());
                 assert!(node.right.is_none());
-            },
+            }
             None => panic!("Root should not be None after insertion"),
         }
     }
-}    
-
-
+}
--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -1,14 +1,13 @@
 /*
-	bfs
-	This problem requires you to implement a basic BFS algorithm
+    bfs
+    This problem requires you to implement a basic BFS algorithm
 */
 
-//I AM NOT DONE
 use std::collections::VecDeque;
 
 // Define a graph
 struct Graph {
-    adj: Vec<Vec<usize>>, 
+    adj: Vec<Vec<usize>>,
 }
 
 impl Graph {
@@ -21,20 +20,38 @@ impl Graph {
 
     // Add an edge to the graph
     fn add_edge(&mut self, src: usize, dest: usize) {
-        self.adj[src].push(dest); 
-        self.adj[dest].push(src); 
+        self.adj[src].push(dest);
+        self.adj[dest].push(src);
     }
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
+        let n = self.adj.len();
+        if start >= n {
+            // For out-of-bounds start indices, return empty order.
+            return Vec::new();
+        }
 
-        let mut visit_order = vec![];
+        let mut deque = VecDeque::with_capacity(n);
+        let mut visited = vec![false; n];
+        let mut visit_order = Vec::with_capacity(n);
+
+        visited[start] = true;
+        deque.push_back(start);
+
+        while let Some(u) = deque.pop_front() {
+            visit_order.push(u);
+            for &v in &self.adj[u] {
+                if !visited[v] {
+                    visited[v] = true;
+                    deque.push_back(v);
+                }
+            }
+        }
+
         visit_order
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -84,4 +101,3 @@ mod tests {
         assert_eq!(visited_order, vec![0]);
     }
 }
-
